b7961ce0ac0e52d7281575ea5df2f1497ea30bb6
SebastienLouis <sebastien.louis.pro@gmail.com>
2016-02-20 20:30:09 +0100

    Categories screen implemented and products in progress

--- Xmazon/Views/ListCategories.cs
+++ Xmazon/Views/ListCategories.cs
@@ -6,18 +6,22 @@ using System.Collections;
 
 namespace Xmazon
 {
-	public partial class ListStores : ContentPage
+	public partial class ListCategories : ContentPage
 	{
-		private ArrayList storesList = new ArrayList();
-		public ListStores ()
+		private ArrayList categoriesList = new ArrayList();
+		private Store selectedStore;
+
+		public ListCategories (Store store)
 		{
+			selectedStore = store;
+
 			InitializeComponent ();
 			initializeStoresList ();
 		}
 
 		private async void initializeStoresList(){
-				
-			string url = XmazonRequest.STORE_LIST;
+
+			string url = XmazonRequest.CATEGORY_LIST;
 			var method = XmazonRequest.Method.GET;
 
 			var requestObject = new XmazonRequest ();
@@ -25,20 +29,24 @@ namespace Xmazon
 			var headers = new Dictionary<string, string> ();
 			headers.Add ("Authorization", "Bearer " + AppContext.AccessToken);
 
-			var requestResult = await requestObject.Call (url, method, null, null, headers);
+
+			var getParams = new Dictionary<string, string> ();
+			getParams.Add ("store_uid", selectedStore.uid);
+
+			var requestResult = await requestObject.Call (url, method, getParams, null, headers);
 
 			if (requestResult.ContainsKey ("result")) {
-				var jsonStoresList = requestResult ["result"];
-				foreach (JsonValue store in jsonStoresList){
-					Store currentStore = Store.Deserialize(store);
-					storesList.Add(currentStore);
+				var jsonCategoriesList = requestResult ["result"];
+				foreach (JsonValue category in jsonCategoriesList){
+					Category currentCategory = Category.Deserialize(category);
+					categoriesList.Add(currentCategory);
 				}
-				lstView.ItemsSource = storesList;
+				lstView.ItemsSource = categoriesList;
 				lstView.ItemTemplate = new DataTemplate(typeof(TextCell));
 				lstView.ItemTemplate.SetBinding(TextCell.TextProperty, "name");
 				lstView.ItemSelected += async (sender, e) => {
-					Store selectedStore = (Store)e.SelectedItem;
-					Navigation.InsertPageBefore (new ListCategories(selectedStore), this);
+					Category selectedCategory = (Category)e.SelectedItem;
+					Navigation.InsertPageBefore (new ListProducts(selectedCategory), this);
 					await Navigation.PopAsync ();
 				};
 			}
---
+++ Xmazon/Views/ListProducts.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Json;
+using Xamarin.Forms;
+using System.Collections;
+
+namespace Xmazon
+{
+	public partial class ListProducts : ContentPage
+	{
+		private ArrayList productsList = new ArrayList();
+		private Category selectedCategory;
+
+		public ListProducts (Category category)
+		{
+			selectedCategory = category;
+			InitializeComponent();
+			initializeProductsList ();
+		}
+
+		private async void initializeProductsList(){
+
+			string url = XmazonRequest.PRODUCT_LIST;
+			var method = XmazonRequest.Method.GET;
+
+			var requestObject = new XmazonRequest ();
+
+			var headers = new Dictionary<string, string> ();
+			headers.Add ("Authorization", "Bearer " + AppContext.AccessToken);
+
+
+			var getParams = new Dictionary<string, string> ();
+			getParams.Add ("category_uid", selectedCategory.uid);
+
+			var requestResult = await requestObject.Call (url, method, getParams, null, headers);
+
+			if (requestResult.ContainsKey ("result")) {
+				var jsonProductsList = requestResult ["result"];
+				foreach (JsonValue product in jsonProductsList){
+					Product currentProduct = Product.Deserialize(product);
+					productsList.Add(currentProduct);
+				}
+				lstView.ItemsSource = productsList;
+				lstView.ItemTemplate = new DataTemplate(typeof(TextCell));
+
+			}
+		}
+
+
+		async void OnLogoutButtonClicked (object sender, EventArgs e)
+		{
+			/*
+			 * 
+			 * Webservice de déconnexion
+			 * 
+			 */ 
+			//App.IsUserLoggedIn = false;
+			Navigation.InsertPageBefore (new Connexion (), this);
+			await Navigation.PopAsync ();
+		}
+	}
+}
+
